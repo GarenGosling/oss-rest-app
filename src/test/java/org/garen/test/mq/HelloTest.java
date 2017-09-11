c86d8b10500672ca8a0a103ffbe9580dd5c48b32
liuxueliang <liuxueliang@yingu.com>
2017-09-11 17:15:58 +0800

    init project

---
+++ src/test/java/org/garen/test/mq/HelloTest.java
@@ -0,0 +1,20 @@
+package org.garen.test.mq;
+
+
+import org.garen.oss.rpc.mq.publish.HelloSender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloTest {
+    @Autowired
+    private HelloSender sender;
+    @Test
+    public void hello() throws Exception {
+        sender.send();
+    }
+}
