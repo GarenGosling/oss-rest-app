07c613070185c17fa3e9721da0a47b25a7050c4f
GarenGosling <liuxueliang@garen.org>
2017-09-21 22:33:41 +0800

    解决跨域问题

--- src/main/java/org/garen/OssRun.java
+++ src/main/java/org/garen/OssRun.java
@@ -1,9 +1,14 @@
 package org.garen;
 
+import org.garen.oss.swagger.ApiOriginFilter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+
+import javax.servlet.Filter;
 
 
 /**
@@ -13,6 +18,29 @@ import org.springframework.stereotype.Controller;
 @SpringBootApplication
 @MapperScan("org.garen.oss.mybatis.mapper")
 public class OssRun {
+
+    /**
+     * 配置过滤器
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean someFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(apiOriginFilter());
+        registration.addUrlPatterns("/*");
+        registration.addInitParameter("paramName", "paramValue");
+        registration.setName("apiOriginFilter");
+        return registration;
+    }
+
+    /**
+     * 创建一个bean
+     * @return
+     */
+    @Bean(name = "apiOriginFilter")
+    public Filter apiOriginFilter() {
+        return new ApiOriginFilter();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(OssRun.class, args);
